0f79b5f8f5733ab7395be95c8bc23d057049ec51
rikki cattermole <pbdadmin@gmail.com>
2015-01-20 11:04:37 +1300

    Woops last commit should have included this.

--- source/wrappers/djvm/jvm.d
+++ source/wrappers/djvm/jvm.d
@@ -85,53 +85,84 @@ final class DJvm {
 			if (DerelictJvm.isLoaded)
 				return true;
 
-			if (vmLoc == "") {
-				import std.process : execute;
-				import std.algorithm : reverse;
-				import std.path : buildNormalizedPath;
-				import std.file : exists;
-				auto java = execute(["java", "-verbose", "-version"]);
-				
-				char[] temp;
-				ubyte stepEnd;
-				
-				foreach_reverse(c; java.output) {
-					if (c == 'm') {
-						stepEnd = 0;
-					} else if (c == 'o' && stepEnd == 0) {
-						stepEnd = 1;
-					} else if (c == 'r' && stepEnd == 1) {
-						stepEnd = 2;
-					} else if (c == 'f' && stepEnd == 2) {
-						temp = temp[0 .. $-4];
-						break;
+			bool loadIt(bool withArg=true) {
+				if (vmLoc != "") {
+					try {
+						if (withArg)
+							DerelictJvm.load(vmLoc);
+						else
+							DerelictJvm.load();
+						return true;
+					} catch(Error e) {
+					} catch(Exception e) {
 					}
-					
-					temp ~= c;
 				}
-				temp.reverse;
 				
-				string filePath = buildNormalizedPath(temp, "../../bin/server/jvm.dll");
-				if (!filePath.exists)
-					filePath = buildNormalizedPath(temp, "lib/amd64/server/libjvm.so");
-				
-				// TODO: 32bit version?
-				
-				if (filePath.exists) {
-					vmLoc = filePath;
-				}
+				return false;
 			}
+
+			if (loadIt(false)) return true;
+
+			// fallback!
+
+			import std.process : execute;
+			import std.algorithm : reverse;
+			import std.path : buildNormalizedPath;
+			import std.file : exists;
+			import std.string : strip;
+
+			auto java = execute(["java", "-verbose", "-version"]);
+			// make sure we actually did run java
+			if (java.status != 0 || java.output.length <= 1024)
+				return false;
+
+			char[] temp;
+			ubyte stepEnd;
 			
-			
-			if (vmLoc != "") {
-				try {
-					DerelictJvm.load(vmLoc);
-					return true;
-				} catch(Exception e) {
+			foreach_reverse(c; java.output) {
+				if (c == 'm') {
+					stepEnd = 0;
+				} else if (c == 'o' && stepEnd == 0) {
+					stepEnd = 1;
+				} else if (c == 'r' && stepEnd == 1) {
+					stepEnd = 2;
+				} else if (c == 'f' && stepEnd == 2) {
+					temp = temp[0 .. $-4];
+					break;
 				}
+				
+				temp ~= c;
 			}
+			temp.reverse;
+			temp = temp.strip;
+			if (temp[$-1] == ']')
+				temp.length--;
+
+			// JDK 64bit windows/possible cross platform
+			string filePath = buildNormalizedPath(temp, "../../bin/server/jvm.dll");
+			// TODO: JDK 32bit windows/possible cross platform
+
+			// JRE 64bit windows
+			if (!filePath.exists)
+				filePath = buildNormalizedPath(temp, "../../bin/client/jvm.dll");
+			else { vmLoc = filePath; return loadIt; }
+			// TODO: JRE 32bit windows
+
+			// JDK 64bit linux
+			if (!filePath.exists)
+				filePath = buildNormalizedPath(temp, "lib/amd64/server/libjvm.so");
+			else { vmLoc = filePath; return loadIt; }
+			if (!filePath.exists)
+				filePath = buildNormalizedPath(temp, "amd64/server/libjvm.so");
+			else { vmLoc = filePath; return loadIt; }
+
+			// TODO: JDK 32bit linux
+			// TODO: JRE 64bit linux
+			// TODO: JRE 32bit linux
+			// TODO: JDK 64bit OSX
+			// TODO: JRE 32bit OSX
 			
-			return false;
+			assert(0);
 		}
 
 		/**
@@ -222,9 +253,15 @@ struct JVMArguments {
 }
 
 extern(System) export {
+	private __gshared bool loadSTDFiles;
+
 	jint JNI_OnLoad(JavaVM *vm, void *reserved) {
 		import core.runtime:Runtime;
 		Runtime.initialize();
+
+		JNIEnv* env;
+		(*vm).GetEnv(vm, cast(void**)&env, JNI_VERSION_1_6);
+		DJvm vm_ = new DJvm(vm, env);
 
 		return JNI_VERSION_1_6;
 	}
@@ -236,20 +273,24 @@ extern(System) export {
 }
 
 shared static this() {
-	version(Windows) {
-		import std.stdio: stdin, stdout, stderr, File;
-		import core.sys.windows.windows: GetStdHandle, STD_INPUT_HANDLE, STD_OUTPUT_HANDLE, STD_ERROR_HANDLE;
+	import std.stdio: stdin, stdout, stderr, File;
+	// Loads up new instances of stdin/stdout/stderr if they have not been properly created
 
-		File nstdin;
-		nstdin.windowsHandleOpen(GetStdHandle(STD_INPUT_HANDLE), ['r']);
-		stdin = nstdin;
-		
-		File nstdout;
-		nstdout.windowsHandleOpen(GetStdHandle(STD_OUTPUT_HANDLE), ['w']);
-		stdout = nstdout;
-		
-		File nstderr;
-		nstderr.windowsHandleOpen(GetStdHandle(STD_ERROR_HANDLE), ['w']);
-		stderr = nstderr;
+	if (stdin.error || stdout.error || stderr.error) {
+		version(Windows) {
+			import core.sys.windows.windows: GetStdHandle, STD_INPUT_HANDLE, STD_OUTPUT_HANDLE, STD_ERROR_HANDLE;
+
+			File nstdin;
+			nstdin.windowsHandleOpen(GetStdHandle(STD_INPUT_HANDLE), ['r']);
+			stdin = nstdin;
+			
+			File nstdout;
+			nstdout.windowsHandleOpen(GetStdHandle(STD_OUTPUT_HANDLE), ['w']);
+			stdout = nstdout;
+			
+			File nstderr;
+			nstderr.windowsHandleOpen(GetStdHandle(STD_ERROR_HANDLE), ['w']);
+			stderr = nstderr;
+		}
 	}
 }
